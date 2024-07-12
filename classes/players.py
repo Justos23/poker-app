7418da47e127a2c8127bcc98417008f307342306
Justos23 <justin.depardieu@gmail.com>
2024-07-11 21:04:54 -0400

    First project import

---
+++ classes/players.py
@@ -0,0 +1,194 @@
+import names
+from collections import Counter
+
+
+class Player():
+
+    def __init__(self, id, buy_in, players):
+        self.id = id,
+        self.players = players
+        self.stack = buy_in
+        self.is_fold = False
+        self.hand = []
+        self.name = self.generate_random_name()
+        self.statement = ""
+        self.score = 0
+        self.digit_dict = {'2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9, '10' : 10, 'Jack' : 11, 'Queen' : 12, 'King' : 13, 'Ace' : 14}
+
+    def generate_random_name(self):
+        return names.get_first_name()
+
+
+
+    def get_digit(self, hand):
+            digit =  []
+            for card in range(len(hand)):
+                digit.append(self.digit_dict[hand[card][:-1]])
+            return digit
+
+
+    def get_suit(self, hand):
+        suite = []
+        for card in range(len(hand)):
+            suite.append(hand[card][-1])
+        return suite
+
+
+    def check_flush(self, suit, hand):
+        for key, val in suit.items():
+            if val >= 5:
+                return key
+        return None
+
+
+    def check_straight(self, digits):
+        if 14 in digits:
+            digits.append(1)
+        straight_high = None
+        i = 1
+        sorted_cards = sorted(digits)
+        prev = sorted_cards[0]
+        for d in sorted_cards[1:]:
+            
+            if prev + 1 == d:
+                i += 1
+                if i >= 5:
+                    straight_high = d
+            else:
+                i = 1
+            prev = d
+        return straight_high
+        
+
+    def check_four_of_a_kind(self, digit):
+        for key, val in digit.items():
+            if val == 4:
+                high = max([k for k,v in digit.items() if k != key])
+                return key, [key, high]
+        return None, None
+
+
+    def check_full_house(self, digit):
+        full = []
+        three = [k for k,v in digit.items() if v == 3]
+        two = [k for k,v in digit.items() if v == 2]
+        if three:
+            three = max(three)
+            full.append(three)
+        if two:
+            two = max(two)
+            full.append(two)
+        if len(full) == 2:
+            return full
+        else:
+            return None
+
+
+    def check_three_of_a_kind(self, digit):
+        three = [k for k,v in digit.items() if v == 3]
+        if three:
+            key = max(three)
+            three_of_a_kind = [key]
+            for n in range(2):
+                three_of_a_kind.append(max([k for k,v in digit.items() if k not in three_of_a_kind]))
+            return key, three_of_a_kind
+        else:
+            return None, None
+
+
+    def check_two_pairs(self, digit):
+        two_pairs = [k for k,v in digit.items() if v == 2]
+        if len(two_pairs) >= 2:
+            two_pairs = sorted(two_pairs, reverse=True)[:2]
+            keys = two_pairs
+            high = max([k for k,v in digit.items() if k not in two_pairs])
+            two_pairs.append(high)
+            return keys, two_pairs
+        else:
+            return None, None
+        
+    def check_one_pair(self, digit):
+        pair = [k for k,v in digit.items() if v == 2]
+        if len(pair) > 0:
+            key = pair[0]
+            for n in range(3):
+                pair.append(max([k for k,v in digit.items() if k not in pair]))
+        else:
+            key, pair  = None, None
+        return key, pair
+
+    def get_score(self, result, hand):
+        n = 2
+        for card in hand:
+            result += card/(10**n)
+            n = n + 2
+        return round(result, 10)
+
+    def get_result(self):
+        suit = self.get_suit(self.hand)
+        digits = self.get_digit(self.hand)
+        s = Counter(suit)
+        d = Counter(digits)
+        flush_color = self.check_flush(s, self.hand)
+        if not flush_color == None:
+            flush_hand = [x for x in self.hand if x[-1] == flush_color]
+            flush_digits = self.get_digit(flush_hand)
+            flush_straight_high = self.check_straight(flush_digits)
+            if not flush_straight_high == None:
+                if flush_straight_high == 14:
+                    result = 9
+                    self.statement = f"{self.name} has a Royal flush!"
+                    self.score = result
+                    return
+                else:
+                    result = self.get_score(8, [flush_straight_high])
+                    self.statement = f"{self.name} has a flush straight of high {list(self.digit_dict.values()).index(flush_straight_high)}"
+                    self.score = result
+                    return
+            else:
+                temp_result = self.get_score(5, sorted(flush_digits, reverse=True)) 
+        key, four_of_a_kind = self.check_four_of_a_kind(d)
+        if not four_of_a_kind == None:
+            result = self.get_score(7, four_of_a_kind)
+            self.statement = f"{self.name} has a four of a kind: {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(key)]}"
+            self.score = result
+            return
+        full = self.check_full_house(d)
+        if not full == None:
+            result = self.get_score(6, full)
+            self.statement = f"{self.name} has a full house: {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(full[0])]} by the {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(full[1])]}"
+            self.score = result
+            return
+        if "temp_result" in locals():
+            self.statement = f"{self.name} has a flush of high {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(sorted(flush_digits)[-1])]}"
+            self.score = temp_result
+            return
+        straight_high = self.check_straight(digits)
+        if not straight_high == None:
+            result = self.get_score(4, sorted([straight_high], reverse=True))
+            self.statement = f"{self.name} has a flush straight of high {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(straight_high)]}"
+            self.score = result
+            return
+        key, three_of_a_kind = self.check_three_of_a_kind(d)
+        if not three_of_a_kind == None:
+            result = self.get_score(3, three_of_a_kind)
+            self.statement = f"{self.name} has a three of a kind of {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(key)]}"
+            self.score = result
+            return
+        key, two_pairs = self.check_two_pairs(d)
+        if not two_pairs == None:
+            result = self.get_score(2, two_pairs)
+            self.statement = f"{self.name} has two pairs: {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(key[0])]} and {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(key[1])]}"
+            self.score = result
+            return
+        key, one_pair = self.check_one_pair(d)
+        if not one_pair == None:
+            result = self.get_score(1, one_pair)
+            self.statement = f"{self.name} has one pair of {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(key)]}"
+            self.score = result
+            return
+        else:
+            result = self.get_score(0, sorted(d, reverse=True))
+            self.statement = f"{self.name} has a high of {list(self.digit_dict.keys())[list(self.digit_dict.values()).index(sorted(d, reverse=True)[0])]}"
+            self.score = result
+            return
---
+++ classes/table.py
@@ -0,0 +1,175 @@
+import random
+from numpy.random import choice
+
+class Table():
+
+    def __init__(self, players):
+        self.allplayers = players
+        self.allplayerslist = list(self.allplayers.keys())
+        self.running_players = self.allplayers.copy()
+        self.bb = 25
+        self.sb = 10
+        self.button = 0
+        self.pot = self.bb + self.sb
+        self.init_deck = ['2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JackH', 'QueenH','KingH', 'AceH',\
+                 '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JackD', 'QueenD','KingD', 'AceD',\
+                 '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JackS', 'QueenS','KingS', 'AceS',\
+                 '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JackC', 'QueenC','KingC', 'AceC',]
+        self.deck = self.init_deck.copy()
+        self.cards_in_the_middle = []
+        self.winners = []
+        self.bet = 0
+
+
+
+
+    def draw(self, n):
+        drawn_cards = []
+        for _ in range(n):
+            r = random.randrange(len(self.deck))
+            drawn_cards.append(self.deck[r])
+            self.deck.pop(r)
+        self.cards_in_the_middle.extend(drawn_cards)
+        return drawn_cards
+
+
+    def has_sufficient_funds(self, player, action):
+        if action == 'call':
+            return player.stack >= self.bet
+        elif action == 'raise':
+            return player.stack > self.bet
+
+
+    def call(self, player):
+        if self.has_sufficient_funds(player, 'call'):
+            player.stack -= self.bet
+            spent = self.bet
+            print(f"{player.name} is calling")
+        else:
+            spent = player.stack
+            player.stack = 0
+            print(f"{player.name} is all-in!")
+        return spent
+
+    def raise_bet(self, player):
+        if player.stack >= self.bet * 2:
+            if self.bet == 0:
+                spent = self.bb * 2
+            else:
+                spent = self.bet * 2
+            player.stack -= spent
+            print(f"{player.name} is raising: ${spent}")
+        else:
+            spent = player.stack
+            player.stack = 0
+            print(f"{player.name} puts ${spent} on the table and is all-in!")
+        self.bet = spent
+        return spent
+
+
+    def do_fold(self, player):
+        del self.running_players[player.id[0]]
+        player.is_fold = True
+        return -1
+
+
+    def take_action(self, player):
+        if self.bet == 0:
+            action = choice([-1, 0, "raise"], 1, p=[0.2, 0.5, 0.3])[0]
+        elif self.has_sufficient_funds(player, 'raise') and not self.bet == 0:
+            action = choice([-1, "call", "raise"], 1, p=[0.4, 0.5, 0.1])[0]
+        else:
+            action = choice([-1, "call"], 1, p=[0.5, 0.5])[0]
+
+        if action == "-1":
+            print(f"{player.name} has fold")
+            action = self.do_fold(player)
+        elif action == "0":
+            print(f"{player.name} checks")
+            action = 0
+        elif action == 'call':
+            action = self.call(player)
+        elif action == 'raise':
+            action = self.raise_bet(player)
+        return action
+
+
+    def check_all_good(self, actions):
+        bets = [x for x in actions if x != -1] 
+        return len(list(set(bets))) == 1
+
+
+
+    def end_round(self, winner):
+        winner.stack += self.pot
+        self.reset()
+        return
+
+
+    def play_round(self): 
+        dealer = self.allplayerslist[self.button]
+        print(f"{self.allplayers[dealer].name} has the button")
+        to_play = self.button + 1
+        all_good = False
+        actions = [None]*len(self.allplayers)
+        while not all_good:
+            for n in range(len(self.allplayers)):
+                player = self.allplayerslist[to_play%len(self.allplayers)]
+                if self.allplayers[player].is_fold:
+                    actions[to_play%len(self.allplayers)] = -1
+                    to_play += 1
+                    continue
+                print(f"{self.allplayers[player].name} to play")
+                action = self.take_action(self.allplayers[player])
+                actions[to_play%len(self.allplayers)] = action
+                if action == -1:
+                    if len(self.running_players) == 1:
+                        return 
+                else:
+                    self.pot += action
+                if self.check_all_good(actions):
+                    all_good = True
+                    break
+                to_play += 1
+        self.reset_bet()
+        return 
+
+
+    def get_winner(self):
+        highest = [["None"], 0]
+        for player in self.running_players:
+            highest_score = highest[1]
+            if self.running_players[player].score > highest_score:
+                highest = [[self.running_players[player]], self.running_players[player].score]
+            elif self.running_players[player].score == highest_score:
+                highest[0].append(self.running_players[player])
+            else:
+                continue
+        self.winners = highest[0]
+        return
+
+
+    def reset_bet(self):
+        self.bet = 0
+        return
+
+
+    def reset(self):
+        self.reset_deck()
+        self.reset_players()
+        self.change_button()
+
+
+    def reset_deck(self):
+        self.deck = self.init_deck
+        return 
+
+
+    def reset_players(self):
+        self.running_players = self.allplayers
+        return 
+
+
+    def change_button(self):
+        self.button = (self.button+1)%len(self.allplayers)
+        return
